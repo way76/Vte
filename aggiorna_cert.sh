3615212460eb40a7c6670844c7453f9807ae1fcc
way76 <43696467+way76@users.noreply.github.com>
2022-05-30 09:21:52 +0200

    Create aggiorna_cert.sh

---
+++ aggiorna_cert.sh
@@ -0,0 +1,17 @@
+
+sostituzione_certificato() {
+
+cd /var/www/html/azienda/ssl/
+rm /var/www/html/$1/ssl/$1.logikadev.it.crt
+rm /var/www/html/$1/ssl/$1.logikadev.it.key
+
+wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.crt  > /var/www/html/$1/ssl/$1.logikadev.it.crt
+wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.key  > /var/www/html/$1/ssl/$1.logikadev.it.key
+
+a2enmod ssl
+systemctl restart apache2
+
+}
+
+
+sostituzione_certificato "logika"
