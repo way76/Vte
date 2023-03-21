cb14d290c42b805c2b55adca745cf6439de89dd7
way76 <43696467+way76@users.noreply.github.com>
2023-03-21 10:06:04 +0100

    Update aggiorna_cert.sh

--- aggiorna_cert.sh
+++ aggiorna_cert.sh
@@ -8,7 +8,7 @@ rm /var/www/html/$1/ssl/$1.logikadev.it.key
 wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.crt  > /var/www/html/$1/ssl/$1.logikadev.it.crt
 wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.key  > /var/www/html/$1/ssl/$1.logikadev.it.key
 
-get -qO - https://raw.githubusercontent.com/way76/vte/master/logikasoftware-cloud.crt  > /var/www/html/$1/ssl/logikasoftware-cloud.crt
+wget -qO - https://raw.githubusercontent.com/way76/vte/master/logikasoftware-cloud.crt  > /var/www/html/$1/ssl/logikasoftware-cloud.crt
 wget -qO - https://raw.githubusercontent.com/way76/vte/master/logikasoftware-cloud.key  > /var/www/html/$1/ssl/logikasoftware-cloud.key
 
 
