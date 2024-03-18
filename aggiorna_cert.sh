5f0782fbe5e8bfb159ec6f6a898d660198ae3ac2
way76 <43696467+way76@users.noreply.github.com>
2024-03-18 09:42:57 +0100

    Update aggiorna_cert.sh

--- aggiorna_cert.sh
+++ aggiorna_cert.sh
@@ -2,11 +2,11 @@
 sostituzione_certificato() {
 
 cd /var/www/html/$1/ssl/
-rm /var/www/html/$1/ssl/$1.logikadev.it.crt
-rm /var/www/html/$1/ssl/$1.logikadev.it.key
+rm /var/www/html/$1/ssl/$1.logik*
 
-wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.crt  > /var/www/html/$1/ssl/$1.logikadev.it.crt
-wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.key  > /var/www/html/$1/ssl/$1.logikadev.it.key
+
+#wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.crt  > /var/www/html/$1/ssl/$1.logikadev.it.crt
+#wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.key  > /var/www/html/$1/ssl/$1.logikadev.it.key
 
 wget -qO - https://raw.githubusercontent.com/way76/vte/master/logikasoftware-cloud.crt  > /var/www/html/$1/ssl/logikasoftware-cloud.crt
 wget -qO - https://raw.githubusercontent.com/way76/vte/master/logikasoftware-cloud.key  > /var/www/html/$1/ssl/logikasoftware-cloud.key
