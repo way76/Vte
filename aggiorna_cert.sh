d0077d0f1e4ec1279ee253684cf04bd367fdf12a
way76 <43696467+way76@users.noreply.github.com>
2024-03-18 10:23:02 +0100

    Update aggiorna_cert.sh

--- aggiorna_cert.sh
+++ aggiorna_cert.sh
@@ -2,11 +2,7 @@
 sostituzione_certificato() {
 
 cd /var/www/html/$1/ssl/
-rm /var/www/html/$1/ssl/$1.logik*
-
-
-#wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.crt  > /var/www/html/$1/ssl/$1.logikadev.it.crt
-#wget -qO - https://raw.githubusercontent.com/way76/vte/master/azienda.logikadev.it.key  > /var/www/html/$1/ssl/$1.logikadev.it.key
+rm /var/www/html/$1/ssl/logik*
 
 wget -qO - https://raw.githubusercontent.com/way76/vte/master/STAR_logikasoftware_cloud.crt  > /var/www/html/$1/ssl/logikasoftware-cloud.crt
 wget -qO - https://raw.githubusercontent.com/way76/vte/master/STAR.logikasoftware.cloud_key.txt  > /var/www/html/$1/ssl/logikasoftware-cloud.key
