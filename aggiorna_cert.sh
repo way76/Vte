651e8825e418324dbf91f704542ff3c82264cbee
way76 <43696467+way76@users.noreply.github.com>
2025-10-31 17:16:26 +0100

    Update certificate download URLs in script

--- aggiorna_cert.sh
+++ aggiorna_cert.sh
@@ -4,8 +4,8 @@ sostituzione_certificato() {
 cd /var/www/html/$1/ssl/
 rm /var/www/html/$1/ssl/logik*
 
-wget -qO - https://raw.githubusercontent.com/way76/vte/master/STAR_logikasoftware_cloud.crt  > /var/www/html/$1/ssl/logikasoftware-cloud.crt
-wget -qO - https://raw.githubusercontent.com/way76/vte/master/STAR.logikasoftware.cloud_key.txt  > /var/www/html/$1/ssl/logikasoftware-cloud.key
+wget -qO - https://raw.githubusercontent.com/way76/vte/master/ls_cloud.crt  > /var/www/html/$1/ssl/logikasoftware-cloud.crt
+wget -qO - https://raw.githubusercontent.com/way76/vte/master/ls_cloud_key.txt  > /var/www/html/$1/ssl/logikasoftware-cloud.key
 
 
 a2enmod ssl
