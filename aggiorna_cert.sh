9bb6ee446232a8b30867fd06302ac5beab7c4a43
way76 <43696467+way76@users.noreply.github.com>
2025-10-31 17:18:43 +0100

    Update wget commands to fetch new certificate files

--- aggiorna_cert.sh
+++ aggiorna_cert.sh
@@ -4,8 +4,8 @@ sostituzione_certificato() {
 cd /var/www/html/$1/ssl/
 rm /var/www/html/$1/ssl/logik*
 
-wget -qO - https://raw.githubusercontent.com/way76/vte/master/ls_cloud.crt  > /var/www/html/$1/ssl/logikasoftware-cloud.crt
-wget -qO - https://raw.githubusercontent.com/way76/vte/master/ls_cloud_key.txt  > /var/www/html/$1/ssl/logikasoftware-cloud.key
+wget -qO - https://raw.githubusercontent.com/way76/vte/master/ls_cloud.file1  > /var/www/html/$1/ssl/logikasoftware-cloud.crt
+wget -qO - https://raw.githubusercontent.com/way76/vte/master/ls_cloud_key.file2  > /var/www/html/$1/ssl/logikasoftware-cloud.key
 
 
 a2enmod ssl
